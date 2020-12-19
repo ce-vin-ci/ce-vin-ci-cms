a6f75808439f092f8d23bee7536f3c7620672185
avergnaud <a.vergnaud@gmail.com>
2020-12-19 12:49:54 +0100

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '0fda3296433a0f0a8823a745526476b5'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c2673b60-997b-4f81-aba0-8472f0dee331'
+};

6eeddc4f55fd57f80b35a506a51d39e0dd9de7aa
avergnaud <a.vergnaud@gmail.com>
2020-12-21 19:35:11 +0100

    déploiement suite à contribution dans le cms

--- api/restaurant/models/restaurant.js
+++ api/restaurant/models/restaurant.js
@@ -1,8 +1,54 @@
 'use strict';
 
+const axios = require('axios')
+require('dotenv').config()
+
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+let notifyGithub = () => {
+    axios.post('https://api.github.com/repos/ce-vin-ci/ce-vin-ci-static/dispatches',
+    {
+        event_type: 'content-updated'
+    },
+    {
+        headers: {
+            'Authorization': `token ${process.env.GITHUB_PAT || ''}`
+        }
+    })
+    .then(function (response) {
+        console.log(response);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+}
+
+/**
+ * Lifecycle callbacks
+ */
+module.exports = {
+
+    /**
+     * Triggered before creation.
+     */
+    lifecycles: {
+
+        afterCreate: async (result, data) => {
+            console.log('afterCreate')
+            notifyGithub()
+        },
+
+        afterUpdate: async (result, params, data) => {
+            console.log('afterUpdate')
+            notifyGithub()
+        },
+
+        afterDelete: async (result, params) => {
+            console.log('afterDelete')
+            notifyGithub()
+        },
+    },
+};
